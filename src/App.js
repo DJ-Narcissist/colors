130353327a8c8da93cfee748da707bd065149b44
Christian Wise <djnarcissist@Christians-MBP.lan>
2023-12-03 09:24:41 -0500

    colors3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import ColorRoutes from './Routes';
 
 function App() {
   return (
-    <div className="App">
-     <Router>
+     
       <div>
         <ColorRoutes />
       </div>
-     </Router>
-    </div>
+    
   );
 };
 
--- src/Routes.js
+++ src/Routes.js
@@ -1,47 +1,48 @@
 import React, { useEffect, useState } from "react";
-import { Route, Navigate, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 import ColorList from "./ColorList";
 import ColorDetails from "./ColorDetails";
 import NewColorForm from "./NewColorForm";
 
 const ColorRoutes = () => {
-    // REtrieve colors form local storage or use default colors
-    const intialColors = JSON.parse(localStorage.getItem("colors")) || {
+    // Retrieve colors from local storage or use default colors
+    const storedColors = localStorage.getItem("colors");
+    const initialColors = storedColors ? JSON.parse(storedColors) : {
         red: "#FF0000",
-        blue:"#0000FF",
+        blue: "#0000FF",
         green: "#00FF00"
     };
+    
     // State to manage colors
-    const [colors, updateColors] = useState(intialColors);
+    const [colors, updateColors] = useState(initialColors);
 
-    // UPdate localStorage whenever colors change
+    // Update localStorage whenever colors change
     useEffect(() => {
-        localStorage.getItem("colors", JSON.stringify(colors));
+        localStorage.setItem("colors", JSON.stringify(colors));
         console.log("Colors updated", colors);
     }, [colors]);
 
-    
-    
-  return (
-    <Router>
-        <Routes>
-            {/* Route for displaying the list of colors */}
-        <Route exact path="/colors">
-            <ColorList />
-        </Route>
-        {/* Route for displaying details of a specific color */}
-        <Route exact path="/colors/:color">
-            <ColorDetails  colors={colors}/>
-        </Route>
-        {/* Route for adding a new color */}
-        <Route exact path="/colors/new">
-            <NewColorForm updateColors = {updateColors} />
-        </Route>
-        {/* Default Route */}
-        <Navigate to="/colors" />
-        </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* Route for displaying the list of colors */}
+                <Route exact path="/colors">
+                    <ColorList colors={colors} />
+                </Route>
+                {/* Route for displaying details of a specific color */}
+                <Route exact path="/colors/:color">
+                    <ColorDetails colors={colors} />
+                </Route>
+                {/* Route for adding a new color */}
+                <Route exact path="/colors/new">
+                    <NewColorForm updateColors={updateColors} />
+                </Route>
+                {/* Default Route */}
+                <Navigate to="/colors" />
+            </Routes>
+        </Router>
+    );
 };
 
 export default ColorRoutes;
